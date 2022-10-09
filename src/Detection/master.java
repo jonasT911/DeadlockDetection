463e2e13f56f1632ce950cd0a5cd1cf1cfbabcd5
Jonas Traweek <jonastraweek@yahoo.com>
2022-10-09 11:48:44 -0500

    Added my project

---
+++ src/Detection/master.java
@@ -0,0 +1,10 @@
+package Detection;
+
+public class master {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Start Deadlock Detection");
+	}
+
+}
