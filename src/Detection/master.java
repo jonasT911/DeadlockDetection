3dcfcca19c9ef0583ac934a4224aadd412768b9a
Jonas Traweek <jonastraweek@yahoo.com>
2022-10-09 11:57:03 -0500

    Added file input

--- src/Detection/master.java
+++ src/Detection/master.java
@@ -1,10 +1,28 @@
 package Detection;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class master {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Start Deadlock Detection");
+		
+		   try {
+			      File myObj = new File("Input.txt");
+			      Scanner myReader = new Scanner(myObj);
+			      while (myReader.hasNextLine()) {
+			        String data = myReader.nextLine();
+			        System.out.println(data);
+			      }
+			      myReader.close();
+			    } catch (FileNotFoundException e) {
+			      System.out.println("An error occurred.");
+			      e.printStackTrace();
+			    }
+		
 	}
 
 }
