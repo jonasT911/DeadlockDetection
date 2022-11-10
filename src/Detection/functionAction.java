5c2aefc7547323f6d045090553be30d8d37a2dda
jtt2279 <jonastraweek@yahoo.com>
2022-11-10 14:22:18 -0600

    Intakes from file and outputs list of locks, functions called, and arguements

--- src/Detection/functionAction.java
+++ src/Detection/functionAction.java
@@ -5,21 +5,45 @@ import java.util.ArrayList;
 public class functionAction {
 	String className;
 	String functionName;
+	ArrayList<String> passedArgs;
 	ArrayList<String> locksAcquired;
 	ArrayList<String> functionsCalled;
 
 	public functionAction(String className, String functionName) {
-		this.className=className;
-		this.functionName=functionName;
+		this.className = className;
+		this.functionName = functionName;
 		locksAcquired = new ArrayList<>();
 		functionsCalled = new ArrayList<>();
+		passedArgs = new ArrayList<>();
 	}
 
 	public void addLock(String lockName) {
+		for (int i = 0; i < passedArgs.size(); i++) {
+			if (lockName.equals(passedArgs.get(i))) {
+				locksAcquired.add(i+"");
+				return;
+			}
+		}
 		locksAcquired.add(lockName);
 	}
 
 	public void addFunction(String function) {
+		
 		functionsCalled.add(function);
+	}
+
+	public void setArgs(String args) {
+		String temp = args.replace("  ", " ");
+		temp = temp.replace("\t", "");
+	
+		while (temp.indexOf(',') != -1) {
+			passedArgs.add(temp.substring(temp.indexOf(' ')+1, temp.indexOf(',')));
+			temp = temp.substring(temp.indexOf(',') + 1);
+		}
+		temp = temp.substring(temp.indexOf(',') + 1);
+		if(temp.indexOf(' ')==0) {
+			temp=temp.substring(1);
+		}
+		passedArgs.add(temp.substring(1+ temp.indexOf(' ')));
 	}
 }
--- src/Detection/master.java
+++ src/Detection/master.java
@@ -12,47 +12,77 @@ public class master {
 		System.out.println("Start Deadlock Detection");
 		int openBrackets = 0;
 		functionAction funct = new functionAction("Fake", "Fake");
-		String currentClass="";
-		ArrayList<functionAction> thisProgram = new ArrayList<functionAction>();
-		try {
-			File myObj = new File("MainFile.java");
-			Scanner myReader = new Scanner(myObj);
-			while (myReader.hasNextLine()) {
-				String data = myReader.nextLine();// Note, this will need to be change to key off of ; and '{' and not \r
-						
-				String tempClass=getClassName(data);
-				if(tempClass!=null) {
-					currentClass=tempClass;
-				}
-				if (isFunctionDefinition(data, openBrackets)) {
-					funct = new functionAction(currentClass, getFunctionName(data));
-				}
+		String currentClass = "";
+	
 
-				if (data.contains("{")) {
-					openBrackets++;
-				}
-				if (data.contains("}")) {
-					openBrackets--;
-					if (openBrackets == 1) {
+		File directoryPath = new File(System.getProperty("user.dir"));
+		// List of all files and directories
+		String contents[] = directoryPath.list();
+		System.out.println("List of files and directories in the specified directory:");
+		for (int j = 0; j < contents.length; j++) {
+			System.out.println(contents[j]);
+			if (contents[j].contains(".java")) {
+				ArrayList<functionAction> thisProgram = new ArrayList<functionAction>();
+				//Begin reading the file
+				try {
+					File myObj = new File(contents[j]);
+					Scanner myReader = new Scanner(myObj);
+					while (myReader.hasNextLine()) {
+						String data = myReader.nextLine();// Note, this will need to be change to key off of ; and {
+															// and not \r
+						int commentIndex = data.indexOf("//");
+						if (commentIndex != -1) {
+							data = data.substring(0, commentIndex);
+						}
+						String tempClass = getClassName(data);
+						if (tempClass != null) {
+							currentClass = tempClass;
+						}
+						if (isFunctionDefinition(data, openBrackets)) {
 
-						thisProgram.add(funct);
+							funct = new functionAction(currentClass, getFunctionName(data));
+							funct.setArgs(data.substring(data.indexOf('(') + 1, (data.indexOf(')'))));
+						}
+
+						if (data.contains("{")) {
+							openBrackets++;
+						}
+						if (data.contains("}")) {
+							openBrackets--;
+							if (openBrackets == 1) {
+
+								thisProgram.add(funct);
+							}
+						}
+						if (data.contains("synchronized")) {
+							funct.addLock(data.substring(data.indexOf('(') + 1, data.indexOf(')')));
+						}
+						String tempFunction = getFunctionCall(data);
+						if (tempFunction != null) {
+							funct.addFunction(tempFunction);
+						}
+						//System.out.println(data);
+
 					}
+					myReader.close();
+				} catch (FileNotFoundException e) {
+					System.out.println("An error occurred.");
+					e.printStackTrace();
 				}
-				System.out.println(data);
 
+				System.out.println("\n\nThisClass");
+				for (int i = 0; i < thisProgram.size(); i++) {
+					System.out.println(thisProgram.get(i).className + "." + thisProgram.get(i).functionName);
+					System.out.println(thisProgram.get(i).locksAcquired);
+					System.out.println(thisProgram.get(i).functionsCalled);
+					System.out.println(thisProgram.get(i).passedArgs);
+				}
 			}
-			myReader.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("An error occurred.");
-			e.printStackTrace();
-		}
-		for (int i = 0; i < thisProgram.size(); i++) {
-			System.out.println(thisProgram.get(i).className + "." + thisProgram.get(i).functionName);
 		}
 	}
 
 	static boolean isFunctionDefinition(String data, int openBrackets) {
-		if (data.contains("(") && data.contains(")")) {
+		if (data.contains("(") && data.contains(")") && data.contains("{")) {
 			if (openBrackets == 1) {
 				return true;
 			}
@@ -76,15 +106,27 @@ public class master {
 		return data.substring(beginIndex + 1, endIndex);
 	}
 
-	static String getClassName (String data){
-		int beginIndex=data.indexOf("class");
-		if(beginIndex==-1) {
+	static String getClassName(String data) {
+		int beginIndex = data.indexOf("class");
+		if (beginIndex == -1) {
 			return null;
 		}
-		String temp= data.substring(beginIndex+6);
+		String temp = data.substring(beginIndex + 6);
 		return temp.substring(0, temp.indexOf(' '));
 	}
 
+	static String getFunctionCall(String data) {
+		if (!(data.contains("(") && data.contains(")") && data.contains(";"))) {
+			return null;
+		}
+		String temp = data.replace(" ", "");
+		temp = temp.replace("\t", "");
+		// Maybe to work on non static I could have a mapping of strings to the parent
+		// class.
+		temp = temp.substring(0, temp.indexOf('(')); // Only works on static functions. Does not work on nested calls.
+		if (temp.equals("for")) {
+			return null;
+		}
+		return temp;
+	}
 }
-
-
