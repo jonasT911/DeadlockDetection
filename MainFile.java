2363c145e7974f42577cde91e166d0f7bd7e2e4a
jtt2279 <jonastraweek@yahoo.com>
2022-11-17 18:09:58 -0600

    Simpler main file for testing

--- MainFile.java
+++ MainFile.java
@@ -1,40 +1,37 @@
 package DeadlockPackage;
 
+
 public class MainFile implements Runnable {
 	private static int sum = 0;
 
 	static Object lock1 = new Object();
 	static Object lock2 = new Object();
-	static Object c = new Object();
-	final int repetitions = 10000;
+	final int repetitions=10000;
 
+	
 	public void garbage(Object A, Object B) {
 		synchronized (A) {
-			foo(A, B);
+		foo(A,B);
 		}
 	}
-
+	
 	public void foo(Object A, Object B) {
-		synchronized (A) {
+	
 			synchronized (B) {
-				synchronized (c) {
-					for (int i = 0; i < repetitions; i++) {
-						sum++;
-
-					}
+				for (int i = 0; i < repetitions; i++) {
+					sum++;
+				
 				}
-			}
+			
 		}
 	}
 
 	public void bar(Object A, Object B) {
 		synchronized (B) {
-			synchronized (c) {
-				synchronized (A) {
-					for (int i = 0; i < repetitions; i++) {
-						sum--;
-
-					}
+			synchronized (A) {
+				for (int i = 0; i < repetitions; i++) {
+					sum--;
+			
 				}
 			}
 		}
@@ -44,19 +41,16 @@ public class MainFile implements Runnable {
 		synchronized (A) {
 			return sum;
 		}
-		synchronized (B) {
-			System.out.println(sum);
-		}
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		System.out.println(getSum(lock1));
+		
 		for (int x = 0; x < 2; x++) {
-			foo(lock1, lock2);
+			garbage(lock1, lock2);
 			bar(lock1, lock2);
 		}
-
+		System.out.println(getSum(lock1));
 	}
 }
